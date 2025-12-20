1f80705b56bde70e085ee6c8d63a93f891a93183
Evan <evanev7@gmail.com>
2025-12-20 13:03:22 +0000

    format

--- src/exo/master/placement_utils.py
+++ src/exo/master/placement_utils.py
@@ -220,7 +220,9 @@ def get_mlx_jaccl_devices_matrix(
                     matrix[i][j] = conn.source_rdma_iface
                     break
             else:
-                raise ValueError("Current jaccl backend requires all-to-all RDMA connections")
+                raise ValueError(
+                    "Current jaccl backend requires all-to-all RDMA connections"
+                )
 
     return matrix
 
@@ -260,6 +262,8 @@ def get_mlx_jaccl_coordinators(
         logger.warning(
             f"Failed to find directly connected ip between {n} and {coordinator}"
         )
-        raise ValueError("Current jaccl backend requires all participating devices to be able to communicate")
+        raise ValueError(
+            "Current jaccl backend requires all participating devices to be able to communicate"
+        )
 
     return {n: f"{get_ip_for_node(n)}:{coordinator_port}" for n in selected_cycle}
